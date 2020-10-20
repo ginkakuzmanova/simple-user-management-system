e330e8c1a26150dd0126c1768d903277b314bf19
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2020-10-20 14:11:29 +0300

    Edited validation regex

--- user-management-system/src/validation/userSchema.js
+++ user-management-system/src/validation/userSchema.js
@@ -4,12 +4,12 @@ export const UserValidationSchema = yup.object({
   firstName: yup
     .string()
     .min(2, "First name should be at least 2 characters!")
-    .matches(/^[A-Za-z]+$/, "First name should contain only letters.")
+    .matches(/^[A-Za-zа-яА-Я]+$/, "First name should contain only letters.")
     .required("First name is required!"),
   lastName: yup
     .string()
     .min(4, "Last name should be at least 4 characters!")
-    .matches(/^[A-Za-z]+$/, "Last name should contain only letters.")
+    .matches(/^[A-Za-zа-яА-Я]+$/, "Last name should contain only letters.")
     .required("Last name is required!"),
   email: yup
     .string()
